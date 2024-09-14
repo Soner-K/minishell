4ab8e25173b7d50df36bd447a5339d2091641d10
Sumi Seo <sumseo@paul-f4Ar6s9.clusters.42paris.fr>
2024-09-14 16:49:07 +0200

    feat/add-exec-combine

--- src/parsing_test.c
+++ src/parsing_test.c
@@ -6,7 +6,7 @@
 /*   By: sumseo <sumseo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 15:34:04 by sokaraku          #+#    #+#             */
-/*   Updated: 2024/09/12 13:04:37 by sumseo           ###   ########.fr       */
+/*   Updated: 2024/09/14 16:48:12 by sumseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -112,57 +112,70 @@ void	print_tokens(t_tokens *tokens)
 // 	}
 // }
 
-int	main(int ac, char **av, char **env)
+int	main(int argc, char **argv, char **envp)
 {
 	int ret;
 	char *line;
 	char *tmp;
+	t_env *env_list;
+	// t_data *data;
 	t_tokens *head;
 	t_exec *exec;
+	env_list = malloc(sizeof(env_list));
+	if (!env_list)
+		return (0);
+	if (argc > 1)
+		exit_program("Minishell does not take arguments.");
+	if (argv[1] != NULL)
+		exit_program("Minishell does not take arguments.");
+	init_signal();
+	while (42)
+	{
+		line = read_prompt();
+		tmp = line;
+		head = create_tokens(line);
+		if (!head)
+			return (printf("tokens creation error"), free(tmp), FAILURE);
+		// printf("Tokens are : \n");
+		// print_tokens(head);
+		// mark_quotes(head);
+		// printf("Expanding...\n");
+		ret = extract_all(head);
+		if (ret <= 0)
+			return (printf("Error during expand\n"), free_tokens(head),
+				free(tmp), FAILURE);
+		// print_tokens(head);
+		// printf("Checking for unclosed quotes...\n");
+		ret = check_if_closed_quotes(head);
+		if (ret < 0)
+			return (printf("Unclosed quotes\n"), free_tokens(head), free(tmp),
+				FAILURE);
+		// printf("Removing quotes, tokens are : \n");
+		ret = quotes_remover(head);
+		if (ret <= 0)
+			return (printf("Error during quotes removal\n"), free_tokens(head),
+				free(tmp), FAILURE);
+		// print_tokens(head);
+		// printf("Checking for commands...\n");
+		ret = find_cmd_type(head, envp);
+		if (ret <= 0)
+			return (printf("Error during command checking\n"),
+				free_tokens(head), free(tmp), FAILURE);
+		// printf("Checking syntax\n");
+		ret = full_check(&head);
+		if (ret <= 0)
+			return (printf("Syntax error\n"), free_tokens(head), free(tmp),
+				FAILURE);
+		// printf("Tokens are : \n");
+		// print_tokens(head);
+		set_cmds_arrays(&head);
+		// print_tokens(head);
+		exec = new_node_exec();
+		set_node_exec(exec, head);
+		printf("node exec check %s\n", exec->cmd_array[0]);
+		printf("node exec check %s\n", exec->cmd_array[1]);
 
-	(void)ac;
-	(void)av;
-	line = readline(">>> ");
-	tmp = line;
-	head = create_tokens(line);
-	if (!head)
-		return (printf("tokens creation error"), free(tmp), FAILURE);
-	printf("Tokens are : \n");
-	print_tokens(head);
-	mark_quotes(head);
-	printf("Expanding...\n");
-	ret = extract_all(head);
-	if (ret <= 0)
-		return (printf("Error during expand\n"), free_tokens(head), free(tmp),
-			FAILURE);
-	print_tokens(head);
-	printf("Checking for unclosed quotes...\n");
-	ret = check_if_closed_quotes(head);
-	if (ret < 0)
-		return (printf("Unclosed quotes\n"), free_tokens(head), free(tmp),
-			FAILURE);
-	printf("Removing quotes, tokens are : \n");
-	ret = quotes_remover(head);
-	if (ret <= 0)
-		return (printf("Error during quotes removal\n"), free_tokens(head),
-			free(tmp), FAILURE);
-	print_tokens(head);
-	printf("Checking for commands...\n");
-	ret = find_cmd_type(head, env);
-	if (ret <= 0)
-		return (printf("Error during command checking\n"), free_tokens(head),
-			free(tmp), FAILURE);
-	printf("Checking syntax\n");
-	ret = full_check(&head);
-	if (ret <= 0)
-		return (printf("Syntax error\n"), free_tokens(head), free(tmp),
-			FAILURE);
-	printf("Tokens are : \n");
-	print_tokens(head);
-	set_cmds_arrays(&head);
-	print_tokens(head);
-	exec = new_node_exec();
-	set_node_exec(exec, head);
-	free_tokens(head);
-	free(tmp);
+		free_tokens(head);
+		free(tmp);
+	}
 }
