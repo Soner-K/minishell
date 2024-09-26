064dca1fe48514b5d635c023905665ef35cd5c0b
Sumi Seo <sumseo@paul-f4Br7s3.clusters.42paris.fr>
2024-09-26 16:32:17 +0200

    fix/open-multi-heredoc

--- src/pipe/exec_utils.c
+++ src/pipe/exec_utils.c
@@ -6,7 +6,7 @@
 /*   By: sumseo <sumseo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 17:30:11 by sumseo            #+#    #+#             */
-/*   Updated: 2024/09/26 11:55:21 by sumseo           ###   ########.fr       */
+/*   Updated: 2024/09/26 16:25:07 by sumseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,6 @@ void	quit_child(t_exec **cmds_list)
 
 void	init_child(t_exec **cmds_list, char **env_copy)
 {
-	signal(SIGINT, sig_handler_forks);
-	signal(SIGQUIT, sig_handler_quit);
 	if (getfile(cmds_list))
 	{
 		only_redirection(cmds_list);
@@ -30,7 +28,6 @@ void	init_child(t_exec **cmds_list, char **env_copy)
 		{
 			store_or_free(NULL, NULL, false, true);
 			exit(0);
-			// break ;
 		}
 		if ((*cmds_list)->cmd_array && parse_path((*cmds_list)->cmd_array,
 				(*cmds_list)->path))
@@ -71,29 +68,31 @@ void	exec_shell_init(t_exec **exec_list)
 void	exec_shell(t_exec **exec_list, t_env **env_list, char **env_copy,
 		t_data *data)
 {
-	int		builtin_check;
-	pid_t	fork_id;
-	int		status;
+	int	builtin_check;
+	int	status;
 
+	// pid_t	fork_id;
+	signal(SIGINT, sig_handler_forks);
+	signal(SIGQUIT, sig_handler_quit);
 	status = 0;
 	builtin_check = which_builtin(*exec_list);
 	if ((*exec_list)->files_info->infile_info->type == HEREDOC)
 		launch_heredoc(exec_list, data, env_copy, env_list);
-	(*exec_list)->pipe_fdi = -1;
-	(*exec_list)->pipe_fdo = -1;
-	if (builtin_check > 0)
-	{
-		(*exec_list)->old_stdin = dup(STDIN_FILENO);
-		(*exec_list)->old_stdout = dup(STDOUT_FILENO);
-		exec_shell_builtin(exec_list, builtin_check, env_list);
-		exec_shell_init(exec_list);
-	}
-	else
-	{
-		fork_id = fork();
-		if (fork_id == 0)
-			init_child(exec_list, env_copy);
-		else
-			get_status(fork_id, status, data);
-	}
+	// (*exec_list)->pipe_fdi = -1;
+	// (*exec_list)->pipe_fdo = -1;
+	// if (builtin_check > 0)
+	// {
+	// 	(*exec_list)->old_stdin = dup(STDIN_FILENO);
+	// 	(*exec_list)->old_stdout = dup(STDOUT_FILENO);
+	// 	exec_shell_builtin(exec_list, builtin_check, env_list);
+	// 	exec_shell_init(exec_list);
+	// }
+	// else
+	// {
+	// 	fork_id = fork();
+	// 	if (fork_id == 0)
+	// 		init_child(exec_list, env_copy);
+	// 	else
+	// 		get_status(fork_id, status, data);
+	// }
 }
--- src/pipe/handle_heredoc.c
+++ src/pipe/handle_heredoc.c
@@ -6,18 +6,46 @@
 /*   By: sumseo <sumseo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 21:03:52 by sumseo            #+#    #+#             */
-/*   Updated: 2024/09/26 11:52:48 by sumseo           ###   ########.fr       */
+/*   Updated: 2024/09/26 16:28:58 by sumseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	heredoc_count(t_exec *exec)
+{
+	int		count;
+	t_exec	*current;
+
+	count = 0;
+	current = exec;
+	while (current)
+	{
+		if (current->files_info->infile_info->type == HEREDOC)
+			count++;
+		current = current->next;
+	}
+	printf("total count %d\n", count);
+	return (count);
+}
 void	launch_heredoc(t_exec **exec_list, t_data *data, char **env_copy,
 		t_env **env_list)
 {
+	int	total;
+	int	i;
+
 	(void)data;
 	(void)exec_list;
 	(void)env_copy;
 	(void)env_list;
 	printf("HERE DOC LAUNCED\n");
+	total = heredoc_count(*exec_list);
+	i = 0;
+	while (i < total && *exec_list != NULL)
+	{
+		open_heredoc(*exec_list);
+		printf("current heredoc infile %d\n", (*exec_list)->infile);
+		exec_list = &(*exec_list)->next;
+		// (*exec_list)->files_info->infile_info->type = INREDIR;
+	}
 }
